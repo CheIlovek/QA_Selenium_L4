2c348f664a4bb3e8fcd2a2c1cad4984d499e09ac
aleksandr <thealeons@mail.ru>
2024-03-23 10:54:34 +0300

    add widget

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -3,18 +3,20 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import utils.Locators;
+import widdgets.Button;
 
 public class MainPage {
 
     private WebDriver browser;
+    Button javaButton;
 
     public MainPage(WebDriver browser) {
         this.browser = browser;
+        this.javaButton = new Button(browser.findElement(Locators.MainPage.JAVA_BUTTON));
     }
 
     public JavaPage clickJavaButton() {
-        WebElement javaButton = browser.findElement(Locators.MainPage.JAVA_BUTTON);
-        javaButton.click();
+       javaButton.click();
         return new JavaPage(browser);
     }
 
